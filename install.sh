90e87096f04dfa7908655bceb46e791910b4d0a6
Midesi <98613545+midesidotnet@users.noreply.github.com>
2023-12-12 13:03:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ read stalkerpass
 echo ""
 while true; do
     read -p "Do you want to install Ministra direclt from their servers? " yn
-    case $yn in
         [Yy]* ) ministra_source=http://download.ministra.com/downloads/159934057961c4dfe9153ee02d7e3fb1/ministra-5.6.10.zip; break;;
         [Nn]* ) ministra_source=http://download.ministra.com/downloads/159934057961c4dfe9153ee02d7e3fb1/ministra-5.6.9.zip; break;;
         * ) echo "Please answer yes or no.";;
